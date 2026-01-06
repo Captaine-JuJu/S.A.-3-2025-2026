b8a7b9cf48c04d37c80d5997e7d1bc3cb5423c44
FORTIN Victor <victor.fortin078@gmail.com>
2026-01-06 11:18:32 +0100

    meilleur gestion des clées et des nonce

--- src/crypto/chacha20.py
+++ src/crypto/chacha20.py
@@ -5,14 +5,22 @@ class Chacha20:
 
     def __init__(self, message: str, key: bytes = None, nonce: bytes = None):
 
-        self.key = key
-        self.nonce = nonce
         self.message = message
 
         if self.key is None:
-            self.key = os.urandom(256)
+            self.key = os.urandom(32)
+        else:
+            if len(self.key) != 32:
+                raise ValueError("La clée ne fait pas 256 bits")
+            self.key = key
+
         if self.nonce is None:
-            self.nonce = os.urandom(96)
+            self.nonce = os.urandom(16)
+        else:
+            if len(self.nonce) != 16:
+                raise ValueError("Le nonce ne fait pas 96 bits")
+            self.nonce = nonce
+
 
 
     def set_message(self, message: str):
@@ -21,13 +29,14 @@ class Chacha20:
 
     def set_nonce(self, nonce: bytes = None):
         if nonce is None:
-            self.nonce = os.urandom(96)
+            self.nonce = os.urandom(96).hex()
         else:
             self.nonce = nonce
         return
 
     def cryptage_chacha20(self):
         message_byts = self.message.encode("utf-8")
+
 
 
 
