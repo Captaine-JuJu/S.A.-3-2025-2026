f04ff29d55d987a9d432a134901473048bf12d26
FORTIN Victor <victor.fortin078@gmail.com>
2026-01-06 11:56:04 +0100

    finit chacha20.py avec librair

--- src/crypto/chacha20.py
+++ src/crypto/chacha20.py
@@ -1,46 +1,74 @@
 import os
 
+import algorithms
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms
+from cryptography.hazmat.backends import default_backend
 
-class Chacha20:
 
-    def __init__(self, message: str, key: bytes = None, nonce: bytes = None):
+class Chacha20AvecBibli:
 
-        self.message = message
+    def __init__(self, message_a_crypt: str, key: bytes = None, nonce: bytes = None):
 
-        if self.key is None:
+        self.message = message_a_crypt
+
+        if key is None:
             self.key = os.urandom(32)
         else:
-            if len(self.key) != 32:
+            if len(key) != 32:
                 raise ValueError("La clée ne fait pas 256 bits")
             self.key = key
 
-        if self.nonce is None:
+        if nonce is None:
             self.nonce = os.urandom(16)
         else:
-            if len(self.nonce) != 16:
+            if len(nonce) != 16:
                 raise ValueError("Le nonce ne fait pas 96 bits")
             self.nonce = nonce
 
 
-
-
-    def set_message(self, message: str):
-        self.message = message
-        return
+    def set_message(self, new_message: str):
+        """Redéfinit le message à chiffrer"""
+        self.message = new_message
 
     def set_nonce(self, nonce: bytes = None):
+        """Redéfinit le nonce"""
         if nonce is None:
             self.nonce = os.urandom(96).hex()
         else:
             self.nonce = nonce
-        return
 
     def cryptage_chacha20(self):
-        message_byts = self.message.encode("utf-8")
+        """Crypte un message en chacha20"""
+        message_bytes = self.message.encode("utf-8")
+
+        algo = algorithms.ChaCha20(self.key, self.nonce)
+
+        cipher = Cipher(algo, mode=None, backend=default_backend())
+        encrypt = cipher.encryptor()
+
+        return encrypt.update(message_bytes) + encrypt.finalize()
 
 
+    def decrypt_chacha20(self, message_chiffre: bytes) -> str:
+        """
+        Décrypte un mot binaire avec la méthode chacha20
+        :param message_chiffre: Message binaire à décripter
+        :return: message décripter
+        """
+        algo = algorithms.ChaCha20(self.key, self.nonce)
+
+        cipher = Cipher(algo, mode=None, backend=default_backend())
+        decrypt = cipher.encryptor()
+
+        message_claire = decrypt.update(message_chiffre) + decrypt.finalize()
+
+        return message_claire.decode('utf-8')
 
 
 if __name__ == "__main__":
-    chacha = Chacha20("salut")
+    chacha = Chacha20AvecBibli("salut")
+    message_crypt = chacha.cryptage_chacha20()
+    print(message_crypt)
+    message = chacha.decrypt_chacha20(message_crypt)
+    print(message)
 
