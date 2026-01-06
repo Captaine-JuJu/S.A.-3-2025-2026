4440905791c2a12709f109fea0d1f96872f5d9bd
FORTIN Victor <victor.fortin078@gmail.com>
2026-01-06 11:07:15 +0100

    string convert into byts

--- src/crypto/chacha20.py
+++ src/crypto/chacha20.py
@@ -4,9 +4,11 @@ import os
 class Chacha20:
 
     def __init__(self, message: str, key: bytes = None, nonce: bytes = None):
+
         self.key = key
         self.nonce = nonce
         self.message = message
+
         if self.key is None:
             self.key = os.urandom(256)
         if self.nonce is None:
@@ -25,8 +27,10 @@ class Chacha20:
         return
 
     def cryptage_chacha20(self):
-        print("CHACHA")
+        message_byts = self.message.encode("utf-8")
+
 
 
 if __name__ == "__main__":
     chacha = Chacha20("salut")
+
