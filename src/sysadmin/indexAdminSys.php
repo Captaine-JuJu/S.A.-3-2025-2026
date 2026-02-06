be7da1765acb01e85b030568863f09500ef18043
Julian <julian.jjean@gmail.com>
2026-02-06 16:25:26 +0100

    logs (30lignes)

--- src/sysadmin/indexAdminSys.php
+++ src/sysadmin/indexAdminSys.php
@@ -5,30 +5,47 @@ include_once("../fragments/headers.html");
 include("../accesDenied.php");
 ?>
 <header>
-	<h1>Administrateur Système</h1>
+        <h1>Administrateur Système</h1>
 </header>
 <?php
 include_once("../fragments/menuSys.html");
 
-$fp = fopen("../données/connections.csv", "r");
+// connexion au a la base de donnée
+include_once("connexion.php");
 
-fgetcsv($fp);
-echo "<div class='log'>";
-	echo "<table>";
-	echo "<thead>";
-	echo "<th>Login</th>";
-	echo "<th>IP</th>";
-	echo "<th>Date</th>";
-	echo "</thead>";
-	while(($resultA = fgetcsv($fp)) !== false){
 
-		echo "<tr>";
-		echo "<td>".$resultA[0]."</td>";
-		echo "<td>".$resultA[1]."</td>";
-		echo "<td>".$resultA[2]."</td>";
-		echo "</tr>";
-	}
-	echo "</table>";
+    $sql = "SELECT * FROM log";
+    $sqlp = mysqli_prepare($connect, $sql);
+
+    mysqli_stmt_execute($sqlp);
+
+    $result = mysqli_stmt_get_result($sqlp);
+
+
+    $rows = [];
+    while ($ligne = mysqli_fetch_row($result)) {
+        $rows[] = $ligne;
+    }
+    $rows = array_reverse($rows);
+
+
+    echo "<div class='log'>";
+        echo "<table>";
+        echo "<thead>";
+        echo "<th>Login</th>";
+        echo "<th>Rôle</th>";
+        echo "<th>Date</th>";
+        echo "</thead>";
+        $i=0;
+        foreach ($rows as $ligne){
+            if ($i >=30){break;}
+            echo "<tr>";
+            foreach ($ligne as $value) {
+                echo "<td>" . htmlspecialchars($value) . "</td>";
+            }
+            echo "</tr>";
+            $i++;
+        }
+        echo "</table>";
 echo "</div>";
-fclose($fp);
 include_once("../fragments/footers.html");
