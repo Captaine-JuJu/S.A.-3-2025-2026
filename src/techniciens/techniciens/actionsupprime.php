139e13373779e8866aff574bcc4207ceaed2b4fe
Clara.RL <145492446+claraRL@users.noreply.github.com>
2026-02-11 13:24:13 +0100

    Delete src/techniciens/techniciens directory

--- src/techniciens/techniciens/actionsupprime.php
+++
@@ -1,9 +0,0 @@
-<?php
-print_r($_GET);
-echo $_GET["nom"];
-include_once("../connexion.php");
-if(isset($_GET["nom"])){
-    $nom = $_GET["nom"];
-    $sql = "DELETE FROM Devices where Nom='$nom';";
-    $result = mysqli_query($connect, $sql);
-}
