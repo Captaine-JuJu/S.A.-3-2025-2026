80736b0d25a144322851ba567a43fccba268b965
Julian <julian.jjean@gmail.com>
2026-02-27 15:05:33 +0100

    Add files via upload

--- src/crypto/chacha20.py
+++ src/crypto/chacha20.py
@@ -1,81 +1,117 @@
-import os
+def bytesVersEntier (data: bytes) -> int:
+    if len(data) != 4:
+        raise ValueError("data doit etre de 4 octets")
 
-import algorithms
-from cryptography.hazmat.primitives.ciphers import Cipher, algorithms
-from cryptography.hazmat.backends import default_backend
+    res = 0
 
-class Chacha20:
-    def __init__(self):
-        print("ok")
+    for i in range(4):
+        res += data[i] << (i * 8)
 
+    return res
 
-class Chacha20AvecBibli:
+def constanteVersEntier():
+    const = b"expand 32-byte k"
+    mots_32bits = []
 
-    def __init__(self, message_a_crypt: str, key: bytes = None, nonce: bytes = None):
+    for i in range(0,len(const),4):
+        bloc = const[i:i+4]
+        ent = bytesVersEntier(bloc)
+        mots_32bits.append(ent)
 
-        self.message = message_a_crypt
+    return mots_32bits
 
-        if key is None:
-            self.key = os.urandom(32)
-        else:
-            if len(key) != 32:
-                raise ValueError("La clée ne fait pas 256 bits")
-            self.key = key
+def keyVersEntier(key: bytes) -> int:
+    if len(key) != 32:
+        raise ValueError("key doit etre de 32 octets")
+    cle_entier = []
 
-        if nonce is None:
-            self.nonce = os.urandom(16)
-        else:
-            if len(nonce) != 16:
-                raise ValueError("Le nonce ne fait pas 96 bits")
-            self.nonce = nonce
+    for i in range(0,len(key),4):
+        bloc = key[i:i+4]
+        ent = bytesVersEntier(bloc)
+        cle_entier.append(ent)
 
+    return cle_entier
 
-    def set_message(self, new_message: str):
-        """Redéfinit le message à chiffrer"""
-        self.message = new_message
+def nonceVersEntier(nonce: bytes) -> int:
+    if len(nonce) != 12:
+        raise ValueError("nonce doit etre de 12 octets")
 
+    nonce_entier = []
+    for i in range(0,len(nonce),4):
+        bloc = nonce[i:i+4]
+        ent = bytesVersEntier(bloc)
+        nonce_entier.append(ent)
 
-    def set_nonce(self, nonce: bytes = None):
-        """Redéfinit le nonce"""
-        if nonce is None:
-            self.nonce = os.urandom(96).hex()
-        else:
-            self.nonce = nonce
-
-
-    def cryptage_chacha20(self):
-        """Crypte un message en chacha20"""
-        message_bytes = self.message.encode("utf-8")
-
-        algo = algorithms.ChaCha20(self.key, self.nonce)
-
-        cipher = Cipher(algo, mode=None, backend=default_backend())
-        encrypt = cipher.encryptor()
-
-        return encrypt.update(message_bytes) + encrypt.finalize()
+    return nonce_entier
 
 
-    def decrypt_chacha20(self, message_chiffre: bytes) -> str:
-        """
-        Décrypte un mot binaire avec la méthode chacha20
-        :param message_chiffre: Message binaire à décripter
-        :return: message décripter
-        """
-        algo = algorithms.ChaCha20(self.key, self.nonce)
+def rotation(v: int, n:int) -> int:
+    res = ((v << n) | (v >> (32 - n))) & 0xFFFFFFFF
+    return res
 
-        cipher = Cipher(algo, mode=None, backend=default_backend())
-        decrypt = cipher.encryptor()
+def quarter_round(a, b, c, d):
 
-        message_claire = decrypt.update(message_chiffre) + decrypt.finalize()
+    a = (a + b) & 0xFFFFFFFF
+    d = d ^ a
+    d = rotation(d, 16)
 
-        return message_claire.decode('utf-8')
+    c = (c + d) & 0xFFFFFFFF
+    b = b ^ c
+    b = rotation(b, 12)
 
+    a = (a + b) & 0xFFFFFFFF
+    d = d ^ a
+    d = rotation(d, 8)
 
+    c = (c + d) & 0xFFFFFFFF
+    b = b ^ c
+    b = rotation(b, 7)
 
-if __name__ == "__main__":
-    chacha = Chacha20AvecBibli("salut")
-    message_crypt = chacha.cryptage_chacha20()
-    print(message_crypt)
-    message = chacha.decrypt_chacha20(message_crypt)
-    print(message)
+    return a, b, c, d
+
+def round(etat_init: list) -> list:
+    init = list(etat_init)
+    for i in range(10):
+
+        # Round des colonnes
+        init[0], init[4], init[8], init[12] = quarter_round(init[0], init[4], init[8], init[12])
+        init[1], init[5], init[9], init[13] = quarter_round(init[1], init[5], init[9], init[13])
+        init[2], init[6], init[10], init[14] = quarter_round(init[2], init[6], init[10], init[14])
+        init[3], init[7], init[11], init[15] = quarter_round(init[3], init[7], init[11], init[15])
+
+        # Round des diagonales
+        init[0], init[5], init[10], init[15] = quarter_round(init[0], init[5], init[10], init[15])
+        init[1], init[6], init[11], init[12] = quarter_round(init[1], init[6], init[11], init[12])
+        init[2], init[7], init[8], init[13] = quarter_round(init[2], init[7], init[8], init[13])
+        init[3], init[4], init[9], init[14] = quarter_round(init[3], init[4], init[9], init[14])
+
+    for i in range(16):
+        init[i] = (init[i] + etat_init[i]) & 0xFFFFFFFF
+
+    return init
+
+def entierVersBytes (entier: int) -> bytes:
+    octets = []
+
+    res = 0
+
+    for i in range(4):
+        octet = (entier >> (i * 8)) & 0xFF
+        octets.append(octet)
+
+    return bytes(octets)
+
+def chacha20_block(key: bytes, nonce:bytes, counter: int) -> bytes:
+    const = constanteVersEntier()
+    key = keyVersEntier(key)
+    nonce = nonceVersEntier(nonce)
+    bits = b""
+    matrice_etat = const + key + [counter] + nonce
+    print(matrice_etat)
+    etat_final = round(matrice_etat)
+
+    for entier in etat_final:
+        bits += entierVersBytes(entier)
+
+    return bits
 
